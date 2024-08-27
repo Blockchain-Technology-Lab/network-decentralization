a1930db805b106de621190c13f31fae038259d68
Dimitris Karakostas <dimit.karakostas@gmail.com>
2024-08-27 09:40:35 +0100

    Randomize delay between crawl executions

--- crawl.py
+++ crawl.py
@@ -29,9 +29,12 @@ def main():
             print(f'\t{ledger} total time: {hours:02} hours, {mins:02} mins, {secs:02} secs')
         print(7*'----------------\n')
 
-        sleep_duration = randint(0, max(0, 24*60*60 - sum(timings.values())))
-        print(f'Sleeping for {sleep_duration} seconds')
-        time.sleep()
+        sleep_duration = randint(0, int(max(0, 24*60*60 - sum(timings.values()))))
+        hours = int(sleep_duration/(60*60))
+        minutes = int((sleep_duration - hours*60*60)/60)
+        seconds = sleep_duration - minutes*60 - hours*60*60
+        print(f'Sleeping for {hours} hours, {minutes} mins, {seconds} secs')
+        time.sleep(sleep_duration)
 
 
 if __name__ == '__main__':
