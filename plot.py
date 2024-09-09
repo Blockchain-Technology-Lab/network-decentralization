3aafec6507205ca0f08984be2e984bacf5811f21
Dimitris Karakostas <dimit.karakostas@gmail.com>
2024-09-09 14:08:56 +0100

    Add plot functions for graph, response sizes

--- plot.py
+++ plot.py
@@ -1,58 +1,111 @@
+import network_decentralization.helper as hlp
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+import json
+import networkx as nx
 import logging
 
 logging.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%Y/%m/%d %I:%M:%S %p', level=logging.INFO)
 
-LEDGERS = ['bitcoin', 'dogecoin', 'litecoin', 'zcash']
+
+def network_edges():
+    logging.info('Plotting edge graphs')
+
+    for ledger in LEDGERS:
+        output_dir = hlp.get_output_directory()
+        edges = []
+        try:
+            with open(output_dir / 'network_edges' / f'{ledger}.csv') as f:
+                csv_reader = csv.reader(f)
+                next(csv_reader)
+                for source, dest in csv_reader:
+                    if source != dest:
+                        edges.append((source, dest))
+        except FileNotFoundError:
+            return
+
+        G = nx.DiGraph()
+        G.add_edges_from(edges)
+
+        pos = nx.spring_layout(G)
+        nx.draw_networkx_nodes(G, pos, node_size=50)
+        logging.info(f'{ledger} - nodes')
+        # nx.draw_networkx_labels(G, pos)
+        # logging.info('labels')
+        nx.draw_networkx_edges(G, pos, edgelist=G.edges())
+        logging.info(f'{ledger} - edges')
+
+        # plt.show()
+        figure = plt.gcf()  # get current figure
+        figure.set_size_inches(80, 60)
+
+        plt.savefig(f'output/graph_{ledger}.png', bbox_inches='tight', dpi=100)
+
+        logging.info(f'{ledger} - finished')
 
 
 def geo_plot(plot_type):
     for ledger in LEDGERS:
         logging.info(f'Plotting {ledger} {plot_type}')
-        labels, sizes = [], []
-        with open(f'output/{plot_type}_{ledger}.csv') as f:
+        entries = []
+        with open(f'output/{plot_type.lower()}_{ledger}.csv') as f:
             csv_reader = csv.reader(f)
             next(csv_reader)
             for line in csv_reader:
-                label_threshold = 11
-                if len(labels) < label_threshold:
-                    labels.append(f'{line[0]} ({line[1]})')
-                else:
-                    labels.append('')
-                sizes.append(int(line[1]))
+                entries.append([line[0], int(line[1])])
+
+        total_nodes = sum([i[1] for i in entries])
+
+        labels, sizes = [], []
+        for entry in entries:
+            if entry[1] / total_nodes > 0.01:
+                labels.append(f'{entry[0]} ({entry[1]:,})')
+            else:
+                labels.append('')
+            sizes.append(entry[1])
+        legend_labels = [i for i in labels if i]
 
         fig, ax = plt.subplots()
-        ax.pie(sizes, labels=labels, textprops={'fontsize': 80}, counterclock=False, startangle=90)
+        # ax.pie(sizes, labels=labels, textprops={'fontsize': 80}, counterclock=False, startangle=90)
+        ax.pie(sizes, textprops={'fontsize': 80}, counterclock=False, startangle=90)
+        plt.legend(legend_labels, loc='upper right', fontsize=50)
 
-        plt.title(f'{ledger.title()} nodes {plot_type} (Total: {sum(sizes)})', fontsize=130)
+        plt.title(f'{ledger.replace("_", " ").title()} Nodes {plot_type} (Total: {sum(sizes):,})', fontsize=130)
 
         figure = plt.gcf()  # get current figure
         figure.set_size_inches(80, 60)
 
-        plt.savefig(f'output/{plot_type}_{ledger}.png', bbox_inches='tight', dpi=100)
+        plt.savefig(f'output/{plot_type.lower()}_{ledger}.png', bbox_inches='tight', dpi=100)
 
 
 def version_plot():
     for ledger in LEDGERS:
         logging.info(f'Plotting {ledger} version')
-        labels, sizes = [], []
+        entries = []
         with open(f'output/version_{ledger}.csv') as f:
             csv_reader = csv.reader(f)
             next(csv_reader)
             for line in csv_reader:
-                label_threshold = 11
-                if len(labels) < label_threshold:
-                    labels.append(f'{line[0]} ({line[1]})')
-                else:
-                    labels.append('')
-                sizes.append(int(line[1]))
+                entries.append([line[0], int(line[1])])
+
+        total_nodes = sum([i[1] for i in entries])
+
+        labels, sizes = [], []
+        for entry in entries:
+            if entry[1] / total_nodes > 0.01:
+                labels.append(f'{entry[0]} ({entry[1]:,})')
+            else:
+                labels.append('')
+            sizes.append(entry[1])
+        legend_labels = [i for i in labels if i]
 
         fig, ax = plt.subplots()
-        ax.pie(sizes, labels=labels, textprops={'fontsize': 80}, counterclock=False, startangle=90)
+        # ax.pie(sizes, labels=labels, textprops={'fontsize': 80}, counterclock=False, startangle=90)
+        ax.pie(sizes, textprops={'fontsize': 80}, counterclock=False, startangle=90)
+        plt.legend(legend_labels, loc='upper right', fontsize=50)
 
-        plt.title(f'{ledger.title()} nodes version (Total: {sum(sizes)})', fontsize=130)
+        plt.title(f'{ledger.replace("_", " ").title()} nodes version (Total: {sum(sizes):,})', fontsize=130)
 
         figure = plt.gcf()  # get current figure
         figure.set_size_inches(80, 60)
@@ -91,11 +144,11 @@ def ip_type_plot():
     # Adding Xticks
     plt.xlabel('Ledger', fontsize=80)
     plt.ylabel('Number of nodes', fontsize=80)
-    plt.xticks([r + barWidth for r in range(len(ipv4))], [ledger.title() for ledger in ledgers], fontsize=70)
+    plt.xticks([r + barWidth for r in range(len(ipv4))], [ledger.replace('_', ' ').title() for ledger in ledgers], fontsize=70)
     plt.yticks(fontsize=70)
     plt.legend(fontsize=80)
 
-    plt.title('Node address types', fontsize=130)
+    plt.title('Address types (reachable nodes)', fontsize=130)
 
     figure = plt.gcf()  # get current figure
     figure.set_size_inches(80, 60)
@@ -103,7 +156,42 @@ def ip_type_plot():
     plt.savefig('output/ip_type.png', bbox_inches='tight', dpi=100)
 
 
-# geo_plot('geography')
-# geo_plot('asn')
-# ip_type_plot()
+def response_length_plot():
+    logging.info('Plotting by response length')
+
+    with open('output/response_length.json') as f:
+        data = json.load(f)
+
+    for ledger in LEDGERS:
+        logging.info(f'Plotting {ledger}')
+        ledger_data = data[ledger]
+
+        x = [i[0] for i in ledger_data]
+        y = [i[1] for i in ledger_data]
+
+        fig, ax = plt.subplots()
+
+        ax.bar(x, y, width=5)
+
+        plt.title(f'{ledger.replace("_", " ").title()} average response sizes', fontsize=130)
+        plt.xlabel('Addresses in response', fontsize=80)
+        plt.xticks(fontsize=60)
+        plt.ylabel('Nodes', fontsize=80)
+        plt.yticks(fontsize=60)
+
+        figure = plt.gcf()  # get current figure
+        figure.set_size_inches(80, 60)
+
+        plt.savefig(f'output/response_size_{ledger}.png', bbox_inches='tight', dpi=100)
+
+
+LEDGERS = ['bitcoin', 'bitcoin_cash', 'dogecoin', 'litecoin', 'zcash']
+
+geo_plot('Geography')
+geo_plot('ASN')
+ip_type_plot()
 version_plot()
+
+# network_edges()
+
+response_length_plot()
