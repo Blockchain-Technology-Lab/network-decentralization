a7727d862d52785f4b993ee6102d2c8782c167b5
LauraAntunes1 <lauanthol@gmail.com>
2025-01-30 10:53:05 +0000

    Add cleanup_dead_nodes.py
    Add a script that removes dead nodes from the node list. A node is considered dead if the status field of the last packet is set to ‘false’.

---
+++ cleanup_dead_nodes.py
@@ -0,0 +1,35 @@
+import json
+import pathlib
+import os
+import network_decentralization.helper as hlp
+import logging
+
+logging.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%Y/%m/%d %I:%M:%S %p', level=logging.INFO)
+
+def main():
+    LEDGERS = hlp.get_ledgers()
+
+    for ledger in LEDGERS:
+        active = set()
+        logging.info(f'Parsing {ledger}')
+        output_dir = hlp.get_output_directory(ledger)
+        filenames = list(pathlib.Path(output_dir).iterdir())
+        logging.info(f'{ledger} - {len(filenames):,} total nodes')
+        for idx, filename in enumerate(filenames):
+            print(f'{ledger} - parsed {idx:,}/{len(filenames):,} files ({100*idx/len(filenames):.2f}%)', end='\r')
+            if filename.is_file() and not str(filename).endswith('.swp'):
+                with open(filename) as f:
+                    entries = json.load(f)
+                    if (entries[-1])['status']:
+                       active.add(filename)
+
+        non_active = set(filenames) - active
+        logging.info(f'{ledger} - {len(active):,} active nodes')
+        for i in non_active:
+            logging.info(f'{ledger} - {len(non_active):,} never active nodes: {i}')
+
+        for filename in non_active:
+            os.remove(filename)
+
+if __name__ == '__main__':
+    main()
