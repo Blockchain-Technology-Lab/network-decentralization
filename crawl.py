29a4d2b813ea130fd5d0546f30be4cad63d5b520
LauraAntunes1 <lauanthol@gmail.com>
2025-02-07 13:19:14 +0000

    Update crawl.py
    Python scripts are now executed using a bash script, so we no longer need the ‘while True’ and ‘sleep’ commands.

--- crawl.py
+++ crawl.py
@@ -11,30 +11,29 @@ def main():
     ledgers = hlp.get_ledgers()
     shuffle(ledgers)
 
-    while True:
-        timings = {}
-        for ledger in ledgers:
-            start = time.time()
-            crawl_network(ledger)
-            total_time = time.time() - start
-            timings[ledger] = total_time
+    timings = {}
+    for ledger in ledgers:
+        start = time.time()
+        crawl_network(ledger)
+        total_time = time.time() - start
+        timings[ledger] = total_time
 
-        print(7*'----------------\n')
-        for ledger in ledgers:
-            total_time = timings[ledger]
-            days = int(total_time / 86400)
-            hours = int((total_time - days*86400) / 3600)
-            mins = int((total_time - hours*3600 - days*86400) / 60)
-            secs = int(total_time - mins*60 - hours*3600 - days*86400)
-            print(f'\t{ledger} total time: {hours:02} hours, {mins:02} mins, {secs:02} secs')
-        print(7*'----------------\n')
+    print(2*'----------------\n')
+    for ledger in ledgers:
+        total_time = timings[ledger]
+        days = int(total_time / 86400)
+        hours = int((total_time - days*86400) / 3600)
+        mins = int((total_time - hours*3600 - days*86400) / 60)
+        secs = int(total_time - mins*60 - hours*3600 - days*86400)
+        print(f'\tcrawl.py: {ledger} total time: {hours:02} hours, {mins:02} mins, {secs:02} secs')
+    print(2*'----------------\n')
 
-        sleep_duration = randint(0, int(max(0, 24*60*60 - sum(timings.values()))))
-        hours = int(sleep_duration/(60*60))
-        minutes = int((sleep_duration - hours*60*60)/60)
-        seconds = sleep_duration - minutes*60 - hours*60*60
-        print(f'Sleeping for {hours} hours, {minutes} mins, {seconds} secs')
-        time.sleep(sleep_duration)
+#    sleep_duration = randint(0, int(max(0, 24*60*60 - sum(timings.values()))))
+#    hours = int(sleep_duration/(60*60))
+#    minutes = int((sleep_duration - hours*60*60)/60)
+#    seconds = sleep_duration - minutes*60 - hours*60*60
+#    print(f'Sleeping for {hours} hours, {minutes} mins, {seconds} secs')
+#    time.sleep(sleep_duration)
 
 
 if __name__ == '__main__':
