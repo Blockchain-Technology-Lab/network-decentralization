84cdae78377f9d7faa37ba83143c345d881eb192
LauraAntunes1 <lauanthol@gmail.com>
2025-04-21 11:57:05 +0100

    Update distribution.py
    Change new column's name

--- bitcoin/distribution.py
+++ bitcoin/distribution.py
@@ -15,9 +15,9 @@ def org_without_tor(mode, date):
                 total_nodes_without_tor = df[df[f'{mode}'] != 'Tor'][f'{date}'].sum()
                 total_nodes = total_nodes_without_tor + tor_nodes
                 df['Distribution'] = df.apply(lambda row: round((row[f'{date}'] / total_nodes_without_tor) * tor_nodes) if row[f'{mode}'] != 'Tor' else 0, axis=1)
-                df['New'] = df[f'{date}'] + df['Distribution']
+                df['Without_tor'] = df[f'{date}'] + df['Distribution']
                 df_without_tor = df[df[f'{mode}'] != 'Tor']
-                df_without_tor[[f'{mode}', 'New']].to_csv(f'./output/{name}_{ledger}_without_tor.csv', index=False)
+                df_without_tor[[f'{mode}', 'Without_tor']].to_csv(f'./output/{name}_{ledger}_without_tor.csv', index=False)
             except IndexError:
                 logging.info(f'No nodes using Tor')
 
