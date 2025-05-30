82ce5c7f85144c82d70701b4bb788440ab8c25b2
LauraAntunes1 <lauanthol@gmail.com>
2025-05-30 11:43:21 +0100

    Update plot.py
    Now depends on the layer(s) and mode(s) specified in config.yaml.

--- ethereum/plot.py
+++ ethereum/plot.py
@@ -10,10 +10,11 @@ import time
 logging.basicConfig(format='[%(asctime)s] %(message)s', datefmt='%Y/%m/%d %I:%M:%S %p', level=logging.INFO)
 
 
-def geo_plot(plot_type):
-    logging.info(f'Plotting {plot_type}')
+def geo_plot(plot_type, layer):
+    logging.info(f'Plotting {plot_type} - {layer}')
     entries = []
-    with open(f'results/{plot_type.lower()}.csv') as f:
+    output_dir = hlp.get_output_directory()
+    with open(f'{output_dir}/{plot_type.lower()}_{layer}.csv') as f:
         csv_reader = csv.reader(f)
         next(csv_reader)
         for line in csv_reader:
@@ -35,25 +36,21 @@ def geo_plot(plot_type):
     ax.pie(sizes, textprops={'fontsize': 20}, counterclock=False, startangle=90)
     plt.legend(legend_labels, loc='upper right', fontsize=12)
 
-    plt.title(f'Nodes {plot_type} (Total: {sum(sizes):,})', fontsize=32)
+    plt.title(f'Nodes {plot_type} - {layer} (Total: {sum(sizes):,})', fontsize=32)
 
     figure = plt.gcf()  # get current figure
     figure.set_size_inches(20, 15)
 
-    plt.savefig(f'results/{plot_type.lower()}.png', bbox_inches='tight', dpi=100)
+    plt.savefig(f'{output_dir}/{plot_type.lower()}_{layer}.png', bbox_inches='tight', dpi=100)
     plt.close(fig)
 
+LAYERS = hlp.get_layers()
+MODES = hlp.get_mode()
 
 def main():
-    geo_plot('Geography')
-#    geo_plot('ASN')
-#    geo_plot('Org')
-#    ip_type_plot()
-#    version_plot()
-  # network_edges()
-#    response_length_plot()
-
-#        time.sleep(30)
+    for layer in LAYERS:
+        for mode in MODES:
+            geo_plot(mode, layer)
 
 if __name__ == '__main__':
     main()
