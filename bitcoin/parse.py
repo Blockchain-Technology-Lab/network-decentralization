900c1c3108b8577cf1552335e4d23e58fed40cb1
LauraAntunes1 <190111637+LauraAntunes1@users.noreply.github.com>
2026-03-03 16:17:31 +0000

    Implement Tor node redistribution in parse.py instead of distribution.py

--- bitcoin/parse.py
+++ bitcoin/parse.py
@@ -327,6 +327,46 @@ def version(reachable_nodes, mode):
             versions_df.to_csv(f'./output/{name.lower()}_{ledger}.csv', index_label = name)
 
 
+def redistribute_tor_nodes(name, ledger, df, mode):
+    """
+    Redistributes Tor node count proportionally across non-Tor rows.
+    :param name: lowercase version of the mode ('countries' or 'organizations') used in file naming.
+    :param ledger: the ledger name.
+    :param df: the dataframe in which the Tor nodes must be reditributed.
+    :param mode: the mode name (e.g., 'Countries', 'Organizations').
+    """
+    date = datetime.today().strftime('%Y-%m-%d')
+    tor_row = df[df[mode] == 'Tor']
+    if tor_row.empty:
+        logging.info(f"No Tor nodes found in {ledger}.")
+        return
+
+    number_of_tor_nodes = tor_row[date].values[0]  # extract the number of Tor nodes for the given date
+    number_of_total_nodes_without_tor = df[df[f'{mode}'] != 'Tor'][f'{date}'].sum()  # sum of node counts excluding the Tor row
+    number_of_total_nodes = number_of_total_nodes_without_tor + number_of_tor_nodes
+    df['Distribution'] = df.apply(lambda row: round((row[f'{date}'] / number_of_total_nodes_without_tor) * number_of_tor_nodes) if row[f'{mode}'] != 'Tor' else 0, axis=1)  # create a new column 'Distribution' that distributes the Tor nodes proportionally to non-Tor rows
+    df[date] = df[date] + df['Distribution']
+    df_without_tor = df[df[f'{mode}'] != 'Tor']  # filter out the Tor row
+    df_without_tor[[mode, date]].to_csv(f'./output/{name}_{ledger}_without_tor.csv', index=False)  # save the updated DataFrame to a new CSV
+
+
+def without_tor():
+    """
+    Loads a CSV file and calls the redistribute_tor_nodes function.
+    """
+    ledger = 'bitcoin'
+    modes = ['Countries', 'Organizations']
+    for mode in modes:
+        logging.info(f'parse.py: Removing Tor from {ledger} {mode}')
+        name = mode.lower()
+        filename = pathlib.Path(f'./output/{name}_{ledger}.csv')
+        if not filename.is_file():
+            logging.warning(f"File not found: {filename}")
+            return None
+        df = pd.read_csv(filename)
+        redistribute_tor_nodes(name, ledger, df, mode)
+
+
 def cluster_organizations(ledger):
     """
     Clusters organizations in CSV files.
@@ -389,6 +429,8 @@ def main():
             geography(reachable_nodes, ledger, mode)
         if 'Organizations' in MODES:
             cluster_organizations(ledger)
+        if 'bitcoin' in LEDGERS:
+            without_tor()
 
 if __name__ == '__main__':
     main()
