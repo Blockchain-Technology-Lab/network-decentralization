b5bd2ecf76adfe325044b2112311449349fd181e
LauraAntunes1 <190111637+LauraAntunes1@users.noreply.github.com>
2026-03-06 14:51:33 +0000

    Metrics now layer-agnostic

--- bitcoin/network_decentralization/metrics/nakamoto_coefficient.py
+++ bitcoin/network_decentralization/metrics/nakamoto_coefficient.py
@@ -1,10 +1,10 @@
 from network_decentralization.metrics.tau_index import compute_tau_index
 
 
-def compute_nakamoto_coefficient(block_distribution):
+def compute_nakamoto_coefficient(distribution):
     """
-    Calculates the Nakamoto coefficient of a distribution of blocks to entities
-    :param block_distribution: a list of integers, each being the blocks that an entity has produced, sorted in descending order
+    Calculates the Nakamoto coefficient of an entity distribution.
+    :param distribution: list of non-negative counts per entity, sorted in descending order
     :returns: int that represents the Nakamoto coefficient of the given distribution, or None if the data is empty
     """
-    return compute_tau_index(block_distribution, 0.5)
+    return compute_tau_index(distribution, 0.5)
